048608624ff80e70c38413136edfde8996195420
UP250072 <up250072@alumnos.upa.edu.mx>
2025-06-05 12:21:55 -0600

    Inicio del control de versiones

---
+++ Exercise2.py
@@ -0,0 +1,43 @@
+#Day 2: 30 Days of python programming
+print("Level 1 ----------------------------------------------")
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+full_name = input("What is your full name? ")
+country = input("What is the name of your country? ")
+city = input("What is the name of your city? ")
+age = input("What is your age? ")
+year = input("In what year did you born? ")
+is_married = input("Are you married? Yes or no? ")
+is_true = input("Is that true? ")
+is_light_on = input("Is the light on? Yes or no? ")
+print("\nLevel 2 ----------------------------------------------\n")
+print("The class of the variable first_name is: ", type(first_name))
+print("The class of the variable last_name is: ", type(last_name))
+print("The class of the variable full_name is: ", type(full_name))
+print("The class of the variable country is: ", type(country))
+print("The class of the variable city is: ", type(city))
+print("The class of the variable age is: ", type(age))
+print("The class of the variable year is: ", type(year))
+print("The class of the variable is_married is: ", type(is_married))
+print("The class of the variable is_true is: ", type(is_true))
+print("The class of the variable is_light_on is: ", type(is_light_on))
+print("The lenght of your First name is: ", len(first_name))
+print("The lenght of your First name and Last name are: ", len(first_name), "and ", len(last_name))
+num_one = 5
+print("\nThe value of numebr one is: 5, and the value of numnebr two is: 4.")
+num_two = 4
+diff = num_two - num_one
+product = num_two*num_one
+division = num_one/num_two
+print("If we sustract n2 from n1 the result is: ", diff, "\nIf we multiply this numbers we got: ", product, "\nIf we divided n1 by n2 we got: ", division)
+remainder=num_two/num_one
+exp=num_one**num_two
+floor_division=num_one%num_two
+print("Remainder: ", remainder)
+print("Exp: ", exp)
+print("Floor division: ", floor_division)
+print("\nThe radios of a circle is 30 meters.")
+radios_circle=30
+area_of_circle=3.1416*(radios_circle**2)
+circum_of_circle=3.1416*radios_circle
+print("The area of the circle is: ", area_of_circle, "\nThe circumference of the circle is: ", circum_of_circle)
---
+++ Prueba.py
@@ -0,0 +1,5 @@
+
+numero = input("ingresa un numero ")
+numero2 = input("Ingresa otro numero 2")
+
+print(3+3)
